305bf4b04840713a213a4af27803ba24c0faea68
GabrieLempert <81615014+GabrieLempert@users.noreply.github.com>
2021-10-19 16:48:15 +0300

    Delete SpeakGoodbye.js

--- SpeakGoodbye.js
+++
@@ -1,8 +0,0 @@
-(function(Window) {
-    var byeSpeaker = {};
-    var speakWord = "Good Bye";
-    byeSpeaker.speak = function() {
-        console.log(speakWord + " " + byeSpeaker.name);
-    }
-    Window.byeSpeaker = byeSpeaker;
-})(Window);
