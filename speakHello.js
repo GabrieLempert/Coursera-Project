da73993db28b1be48e2245ed0acb94d25e912c7b
GabrieLempert <81615014+GabrieLempert@users.noreply.github.com>
2021-10-19 16:48:04 +0300

    Delete speakHello.js

--- speakHello.js
+++
@@ -1,8 +0,0 @@
-(function(Window) {
-    var helloSpeaker = {};
-    var speakWord = "Hello";
-    helloSpeaker.speak = function() {
-        console.log(speakWord + " " + helloSpeaker.name);
-    }
-    Window.helloSpeaker = helloSpeaker;
-})(Window);
